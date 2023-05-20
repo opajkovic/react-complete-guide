aa6a4d5faf5c799a581b18ddb4e9c7783a9a713d
opajkovic <oljaolja170@gmail.com>
2023-05-20 21:13:45 +0200

    splitting components into multiple components

--- 04-adding-basic-css-styling/src/components/ExpenseItem.jsx
+++ 04-adding-basic-css-styling/src/components/ExpenseItem.jsx
@@ -1,17 +1,11 @@
+import ExpenseDate from './ExpenseDate';
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-  const month = props.expenses.date.toLocaleString('en-Us', {month:'long'});
-  const year = props.expenses.date.toLocaleString('en-US', {day:'2-digit'});
-  const day = props.expenses.date.getFullYear();
-
+ 
     return (
       <div className="expense-item">
-        <div>
-          <div>{month}</div>
-          <div>{year}</div>
-          <div>{day}</div>
-        </div>
+        <ExpenseDate date={props.expenses.date} />
         <div className="expense-item__description">
           <h2>{props.expenses.title}</h2>
           <div className="expense-item__price">{props.expenses.amount}</div>
