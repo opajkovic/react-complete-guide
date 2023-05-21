72d0aef0e7b0276307578681b1fe34a807de4c8c
opajkovic <oljaolja170@gmail.com>
2023-05-21 12:24:51 +0200

    update state

--- 04-adding-basic-css-styling/src/components/Card.jsx
+++ 04-adding-basic-css-styling/src/components/Card.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 import './Card.css';
 
 function Card(props) {
-    console.log(props.className)
-    const classes = 'card ' + props.className;
+  const classes = 'card ' + props.className;
   return (
     <div className={classes}>
       {props.children}
--- 04-adding-basic-css-styling/src/components/ExpenseItem.jsx
+++ 04-adding-basic-css-styling/src/components/ExpenseItem.jsx
@@ -1,16 +1,24 @@
+import React,{ useState }  from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from './Card';
 import "./ExpenseItem.css";
 
 
 function ExpenseItem(props) {
+  const [title, setTitle] = useState(props.expenses.title);
+
+  const clickHandler = () => {
+    setTitle('updated');
+    console.log(title);
+  }
  
     return (
       <Card className="expense-item">
         <ExpenseDate date={props.expenses.date} />
         <div className="expense-item__description">
-          <h2>{props.expenses.title}</h2>
+          <h2>{title}</h2>
           <div className="expense-item__price">{props.expenses.amount}</div>
+          <button onClick={clickHandler}>change</button>
         </div>
       </Card>
     );
